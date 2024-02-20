f495dfd772289d855c97d945f6fb81f19dda2323
Andressa Abrantes <andressasabrantes@gmail.com>
2024-02-20 16:46:21 -0300

    feat: Add Category page in the Navbar

--- InterbikesEcommerce/InterbikesWeb/obj/Debug/net8.0/InterbikesWeb.AssemblyInfo.cs
+++ InterbikesEcommerce/InterbikesWeb/obj/Debug/net8.0/InterbikesWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InterbikesWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9825a447eaa078fb9dce957234c5f1852520654")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a8bcd401f0f281197804c08745d0d9a58a67c76")]
 [assembly: System.Reflection.AssemblyProductAttribute("InterbikesWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InterbikesWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
