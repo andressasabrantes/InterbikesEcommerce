3a8bcd401f0f281197804c08745d0d9a58a67c76
Andressa Abrantes <andressasabrantes@gmail.com>
2024-02-20 16:07:39 -0300

    feat: Add Category Controller and Category View

---
+++ InterbikesEcommerce/InterbikesWeb/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InterbikesWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
