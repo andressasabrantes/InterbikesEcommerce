e9a4fcc6455a2c00da5086233a4e72cf9a4b4742
Andressa Abrantes <andressasabrantes@gmail.com>
2024-02-20 17:08:03 -0300

    chore: Add Seed Category Table

--- InterbikesEcommerce/InterbikesWeb/Models/Data/ApplicationDbContext.cs
+++ InterbikesEcommerce/InterbikesWeb/Models/Data/ApplicationDbContext.cs
@@ -10,5 +10,14 @@ namespace InterbikesWeb.Models.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Bike", DisplayOrder = 1},
+                new Category { Id = 2, Name = "Accessories", DisplayOrder = 2},
+                new Category { Id = 3, Name = "Components", DisplayOrder = 3}
+                );
+        }
     }
 }
--- InterbikesEcommerce/InterbikesWeb/obj/Debug/net8.0/InterbikesWeb.AssemblyInfo.cs
+++ InterbikesEcommerce/InterbikesWeb/obj/Debug/net8.0/InterbikesWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InterbikesWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a8bcd401f0f281197804c08745d0d9a58a67c76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f495dfd772289d855c97d945f6fb81f19dda2323")]
 [assembly: System.Reflection.AssemblyProductAttribute("InterbikesWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InterbikesWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
