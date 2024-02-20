a5778f4868b480c01fa3f732ac95ad7964a0061a
Andressa Abrantes <andressasabrantes@gmail.com>
2024-02-20 13:08:50 -0300

    feat: Data Annotations on the model Category

--- InterbikesEcommerce/InterbikesWeb/Models/Category.cs
+++ InterbikesEcommerce/InterbikesWeb/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace InterbikesWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InterbikesWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
