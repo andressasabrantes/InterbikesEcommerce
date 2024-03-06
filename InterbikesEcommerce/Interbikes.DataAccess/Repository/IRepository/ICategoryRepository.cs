74fffad0dbc967a6489188af2caae836771986e4
Andressa Abrantes <andressasabrantes@gmail.com>
2024-03-06 15:10:57 -0300

    feat: Implementing ICategoryRepository

---
+++ InterbikesEcommerce/Interbikes.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Interbikes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interbikes.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        void Save();
+    }
+}
